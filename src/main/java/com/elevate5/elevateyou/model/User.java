cc4dfdb52c84672937860e67bae2c9932c9e78a4
Dan Scholem <dan.scholem@gmail.com>
2025-09-18 19:10:17 -0400

    User class attributes, constructor, getters/setters

--- src/main/java/com/elevate5/elevateyou/model/User.java
+++ src/main/java/com/elevate5/elevateyou/model/User.java
@@ -1,4 +1,59 @@
 package com.elevate5.elevateyou.model;
 
 public class User {
+
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+    private String profileImageURL;
+
+    public User(int userID, String firstName, String lastName, String email, String passwordHash, String profileImageURL) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.profileImageURL = profileImageURL;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+    public String getProfileImageURL() {
+        return profileImageURL;
+    }
+    public void setProfileImageURL(String profileImageURL) {
+        this.profileImageURL = profileImageURL;
+    }
+
+
 }
