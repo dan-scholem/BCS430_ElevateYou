41fe874dd2744031a25b5056615fae3e36e5b145
Dan Scholem <dan.scholem@gmail.com>
2025-09-19 00:18:55 -0400

    resource management

--- src/main/java/com/elevate5/elevateyou/App.java
+++ src/main/java/com/elevate5/elevateyou/App.java
@@ -30,35 +30,34 @@ public class App extends Application {
     public static void connect(){
         //path to the database
         var url = "jdbc:sqlite:testdatabase.db";
-        Connection conn = null;
-        try{
-            //open connection to database
-            conn = DriverManager.getConnection(url);
-            System.out.println("Connected to database successfully");
+        try (Connection conn = DriverManager.getConnection(url)) {
+            try {
 
-            //statement for queries
-            Statement stmt = conn.createStatement();
+                System.out.println("Connected to database successfully");
 
-            //create table and enter values for testing
-            //stmt.executeUpdate("drop table if exists users");
-            //stmt.executeUpdate("create table users (id integer, name string)");
-            //stmt.executeUpdate("insert into users (id, name) values (1, 'John')");
-            //stmt.executeUpdate("insert into users (id, name) values (2, 'Michael')");
+                //statement for queries
+                Statement stmt = conn.createStatement();
 
-            //execute query selecting all from users table
-            ResultSet rs = stmt.executeQuery("select * from users");
-            //print data to console
-            while(rs.next()){
-                System.out.println(rs.getInt("id") + " " + rs.getString("name"));
-            }
-            stmt.close();
-        } catch (SQLException e){ System.out.println(e.getMessage()); }
-        finally{
-            try{
-                if(conn != null){
-                    conn.close();
+                //create table and enter values for testing
+                //stmt.executeUpdate("drop table if exists users");
+                //stmt.executeUpdate("create table users (id integer, name string)");
+                //stmt.executeUpdate("insert into users (id, name) values (1, 'John')");
+                //stmt.executeUpdate("insert into users (id, name) values (2, 'Michael')");
+
+                //execute query selecting all from users table
+                ResultSet rs = stmt.executeQuery("select * from users");
+
+                //print data to console
+                while (rs.next()) {
+                    System.out.println(rs.getInt("id") + " " + rs.getString("name"));
                 }
-            } catch (SQLException ex){ System.out.println(ex.getMessage()); }
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
