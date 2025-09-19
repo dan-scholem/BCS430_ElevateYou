c9603c63e194f2259c28560f51051da41468af1e
Dan Scholem <dan.scholem@gmail.com>
2025-09-19 00:11:14 -0400

    added database connection and tests

--- src/main/java/com/elevate5/elevateyou/App.java
+++ src/main/java/com/elevate5/elevateyou/App.java
@@ -5,11 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 import java.io.IOException;
 
 public class App extends Application {
 
     public static void main(String[] args) {
+
+        connect();
         launch(args);
     }
 
@@ -20,5 +24,36 @@ public class App extends Application {
         primaryStage.setTitle("ElevateYou");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    //connect to sqlite database
+    public static void connect(){
+        //path to the database
+        var url = "jdbc:sqlite:testdatabase.db";
+        Connection conn = null;
+        try{
+            //open connection to database
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connected to database successfully");
+            //statement for queries
+            Statement stmt = conn.createStatement();
+            //create table and enter values for testing
+            //stmt.executeUpdate("drop table if exists users");
+            //stmt.executeUpdate("create table users (id integer, name string)");
+            //stmt.executeUpdate("insert into users (id, name) values (1, 'John')");
+            //stmt.executeUpdate("insert into users (id, name) values (2, 'Michael')");
+            ResultSet rs = stmt.executeQuery("select * from users");
+            while(rs.next()){
+                System.out.println(rs.getInt("id") + " " + rs.getString("name"));
+            }
+            stmt.close();
+        } catch (SQLException e){ System.out.println(e.getMessage()); }
+        finally{
+            try{
+                if(conn != null){
+                    conn.close();
+                }
+            } catch (SQLException ex){ System.out.println(ex.getMessage()); }
+        }
     }
 }
