e4dfe75b5944186b12e621de8a331e20a20c48bc
Dan Scholem <dan.scholem@gmail.com>
2025-09-23 17:54:48 -0400

    added method for connecting to test database

--- src/main/java/com/elevate5/elevateyou/util/DatabaseConnection.java
+++ src/main/java/com/elevate5/elevateyou/util/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    //connects to sqlite database (database.db)
+    //connects to sqlite production database (database.db)
     public static Connection connect(){
         //path to the database
         var url = "jdbc:sqlite:src/main/resources/database/database.db";
@@ -19,5 +19,20 @@ public class DatabaseConnection {
         }
         return null;
     }
+
+    //connects to sqlite test database (test_database.db)
+    public static Connection connectTestDatabase(){
+        //path to the database
+        var url = "jdbc:sqlite:src/main/resources/database/test_database.db";
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            System.out.println("Connected to database successfully");
+            return conn;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
 
 }
