6a51fbbda73231a72b0492392893020cebaaf4c4
Natasia Cox <natacox10@gmail.com>
2025-09-26 15:21:00 -0400

    Created the Login and Registration UIs with their respective controllers

---
+++ src/main/java/com/elevate5/elevateyou/UserLoginController.java
@@ -0,0 +1,23 @@
+package com.elevate5.elevateyou;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UserLoginController {
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private TextField userEmail;
+
+    @FXML
+    private PasswordField userPassword;
+
+}
+
