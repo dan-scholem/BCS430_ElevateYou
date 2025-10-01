448400cbd28d05c0c442e528412a3af6df3b9b61
Dan Scholem <dan.scholem@gmail.com>
2025-10-01 10:54:06 -0400

    added auth email only

--- src/main/java/com/elevate5/elevateyou/UserLoginController.java
+++ src/main/java/com/elevate5/elevateyou/UserLoginController.java
@@ -1,5 +1,8 @@
 package com.elevate5.elevateyou;
 
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -18,6 +21,21 @@ public class UserLoginController {
 
     @FXML
     private PasswordField userPassword;
+
+    @FXML
+    private void handleSignInButtonAction(ActionEvent event) {
+        try{
+            String email = userEmail.getText().trim();
+            UserRecord userRecord = UserLogin.firebaseAuth.getUserByEmail(email);
+            System.out.println("Successfully authenticated user: " + userRecord.getEmail()
+                    + ", ID: "+ userRecord.getUid());
+        } catch (FirebaseAuthException e) {
+            System.out.println("Invalid email or password");
+        } catch (IllegalArgumentException e){
+            System.out.println("Invalid email or password" + e.getMessage());
+        }
+    }
+
 
 }
 
