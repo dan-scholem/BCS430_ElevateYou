5d02bf3c81f5930c9adbeae9e0ac24fd6c952fb1
Dan Scholem <dan.scholem@gmail.com>
2025-10-01 09:53:08 -0400

    added firebase dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,14 @@ module com.elevate5.elevateyou {
     requires eu.hansolo.tilesfx;
     requires java.sql;
 
+    requires google.cloud.firestore;
+    requires com.google.common;
+    requires com.google.auth.oauth2;
+    requires com.google.api.apicommon;
+    requires firebase.admin;
+    requires google.cloud.core;
+    requires com.google.auth;
+
     opens com.elevate5.elevateyou to javafx.fxml;
     exports com.elevate5.elevateyou;
     opens com.elevate5.elevateyou.view to javafx.fxml;
