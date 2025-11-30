e543006839f94a7f385f27692a94821efca0c2bc
Dan Scholem <dan.scholem@gmail.com>
2025-11-29 23:30:28 -0500

    calorie graph displayed

--- src/main/java/com/elevate5/elevateyou/DashboardController.java
+++ src/main/java/com/elevate5/elevateyou/DashboardController.java
@@ -2,12 +2,15 @@ package com.elevate5.elevateyou;
 
 
 import com.elevate5.elevateyou.session.SessionManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -22,14 +25,17 @@ public class DashboardController {
     private CategoryAxis weightXAxis;
     @FXML
     private NumberAxis weightYAxis;
+    @FXML
+    private LineChart<String, Number> calorieChart;
+    @FXML
+    private CategoryAxis calorieXAxis;
+    @FXML
+    private NumberAxis calorieYAxis;
 
     @FXML
     public void initialize() {
 
         Map<String, Object> weightLog = new LinkedHashMap<>(new TreeMap<>(SessionManager.getSession().getWeightEntryMap()));
-
-        weightXAxis.setLabel("Date");
-        weightYAxis.setLabel("Weight (lbs)");
 
         XYChart.Series<String, Number> series = new XYChart.Series<>();
 
@@ -37,7 +43,6 @@ public class DashboardController {
         long maxWeight = 0;
 
         for (Map.Entry<String, Object> entry : weightLog.entrySet()) {
-            System.out.printf(entry.getKey() + ": " + entry.getValue() + "\n");
             series.getData().add(new XYChart.Data<>(formatDate(entry.getKey()), (Long) entry.getValue()));
             if((Long)entry.getValue() < minWeight){
                 minWeight = (Long)entry.getValue();
@@ -53,6 +58,36 @@ public class DashboardController {
 
         weightChart.getData().add(series);
         weightChart.setLegendVisible(false);
+
+        Map<String, Object> calorieMap = new LinkedHashMap<>(new TreeMap<>(SessionManager.getSession().getCalorieDataMap()));
+
+        XYChart.Series<String, Number> calorieSeries = new XYChart.Series<>();
+
+        long minCals = 5000;
+        long maxCals = 0;
+
+        for (Map.Entry<String, Object> entry : calorieMap.entrySet()) {
+            ObservableList<CaloriesWaterIntakeController.CalorieEntry> entryList = (ObservableList<CaloriesWaterIntakeController.CalorieEntry>) entry.getValue();
+            int totalCals = 0;
+            for (CaloriesWaterIntakeController.CalorieEntry calorieEntry : entryList) {
+                totalCals += calorieEntry.getCalories();
+            }
+            if(totalCals < minCals){
+                minCals = totalCals;
+            }
+            if(totalCals > maxCals){
+                maxCals = totalCals;
+            }
+            calorieSeries.getData().add(new XYChart.Data<>(formatDate(entry.getKey()), totalCals));
+        }
+
+        calorieYAxis.setAutoRanging(false);
+        calorieYAxis.setTickUnit(100);
+        calorieYAxis.setLowerBound(minCals - 100);
+        calorieYAxis.setUpperBound(maxCals + 100);
+
+        calorieChart.getData().add(calorieSeries);
+        calorieChart.setLegendVisible(false);
 
 
     }
