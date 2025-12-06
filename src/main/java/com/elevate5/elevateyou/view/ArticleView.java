f9253a7a98ceabf3579507a69bb601af8b38fdff
Dan Scholem <dan.scholem@gmail.com>
2025-12-06 10:32:35 -0500

    removed old navigation code and unused imports

--- src/main/java/com/elevate5/elevateyou/view/ArticleView.java
+++ src/main/java/com/elevate5/elevateyou/view/ArticleView.java
@@ -1,59 +1,30 @@
 package com.elevate5.elevateyou.view;
 
-import com.elevate5.elevateyou.*;
 import com.elevate5.elevateyou.model.ArticleModel;
 import com.elevate5.elevateyou.session.SessionManager;
 import com.elevate5.elevateyou.viewmodel.ArticleViewModel;
-import com.elevate5.elevateyou.viewmodel.FriendsListViewModel;
 import javafx.animation.FadeTransition;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.web.WebView;
 import javafx.stage.Popup;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
-public class ArticleView extends Application {
+public class ArticleView {
 
-    @FXML
-    private Button appointmentsButton;
-    @FXML
-    private Button calendarButton;
-    @FXML
-    private Button dashButton;
-    @FXML
-    private Button exerciseButton;
-    @FXML
-    private Button foodButton;
-    @FXML
-    private Button journalButton;
-    @FXML
-    private Button logoutButton;
-    @FXML
-    private Button medButton;
-    @FXML
-    private Button sleepButton;
-    @FXML
-    private Button friendsButton;
-    @FXML
-    private Button quotesaffirmationBtn;
+
     @FXML
     private Button dailyArticlesButton;
     @FXML
@@ -240,202 +211,6 @@ public class ArticleView extends Application {
         fadeTransition.setAutoReverse(false);
         fadeTransition.play();
         fadeTransition.setOnFinished(e -> favPopup.hide());
-    }
-
-
-    // Logout
-    @FXML
-    private void logoutUser(ActionEvent event) throws IOException {
-        Stage stage;
-        Alert logoutalert = new Alert(Alert.AlertType.CONFIRMATION);
-        logoutalert.setTitle("Logout");
-        logoutalert.setHeaderText("You are about to logout!");
-        logoutalert.setContentText("Are you sure you want to logout?");
-
-        if (logoutalert.showAndWait().get() == ButtonType.OK) {
-            stage = (Stage) logoutButton.getScene().getWindow();
-            System.out.println("User logged out successfully");
-            SessionManager.closeSession();
-            stage.close();
-            UserLogin.loadUserLoginScene(stage);
-        }
-    }
-
-    @FXML
-    protected void dashboardButtonClick() {
-        try {
-            Stage stage = (Stage) dashButton.getScene().getWindow();
-            Dashboard.loadDashboardScene(stage);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    protected void medicationButtonClick() throws IOException {
-        try {
-            Stage stage = (Stage) medButton.getScene().getWindow();
-            Medication.loadMedTrackerScene(stage);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    protected void calendarButtonClick() throws IOException {
-        try {
-            Stage stage = (Stage) calendarButton.getScene().getWindow();
-            CalendarView.loadCalendarScene(stage);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    protected void foodButtonClick() {
-        try {
-            Stage stage = (Stage) foodButton.getScene().getWindow();
-            CaloriesWaterIntake.loadCaloriesWaterIntakeScene(stage);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    protected void journalButtonClick() throws IOException {
-        try {
-            Stage stage = (Stage) journalButton.getScene().getWindow();
-            JournalEntry.loadJournalScene(stage);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-/*
-    @FXML
-    protected void settingsButtonClick() throws IOException {
-
-        try {
-            Stage stage = (Stage) profileButton.getScene().getWindow();
-
-            UserProfile.loadSettingsScene(stage);
-        } catch (IOException e) {
-
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @FXML
-    public void setNotificationsButton() {
-        try {
-            String uid = (SessionManager.getSession() != null && SessionManager.getSession().getUserID() != null)
-                    ? SessionManager.getSession().getUserID()
-                    : "";
-            String email = (SessionManager.getSession() != null && SessionManager.getSession().getUser() != null)
-                    ? SessionManager.getSession().getUser().getEmail()
-                    : "";
-
-            NotificationService svc = new NotificationService(uid, email);
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/elevate5/elevateyou/Notification.fxml"));
-            Node bell = loader.load();
-
-            NotificationController ctrl = loader.getController();
-            ctrl.setService(svc);
-
-            topRightBar.getChildren().add(bell);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
- */
-
-    // Exercise navigation
-    @FXML
-    private void exerciseButtonClick(ActionEvent event) {
-        System.out.println("[NAV] Exercise clicked"); // visibility in console
-        FXMLLoader loader;
-        try {
-            loader = new FXMLLoader(
-                    getClass().getResource("/com/elevate5/elevateyou/exercise.fxml")
-            );
-            Node view = loader.load();
-
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene((javafx.scene.Parent) view, 900, 600));
-            stage.setTitle("Exercise Tracker");
-            stage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "Failed to open Exercise:\n" + e.getMessage()).showAndWait();
-        }
-    }
-    @FXML
-    public void appointmentButtonClick() throws IOException {
-        try {
-            Stage stage = (Stage) appointmentsButton.getScene().getWindow();
-            AppointmentView.loadAppointmentScene(stage);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    public void sleepButtonClick() {
-        try {
-            Stage s = (Stage) sleepButton.getScene().getWindow();
-            Sleep.loadSleepScene(s);
-        } catch (Exception e) {
-            throw new RuntimeException();
-        }
-    }
-
-    @FXML
-    protected void friendsButtonClick() {
-        try {
-            Stage stage = (Stage) friendsButton.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/elevate5/elevateyou/FriendsListView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            FriendsListView controller = fxmlLoader.getController();
-            controller.setViewModel(new FriendsListViewModel());
-            stage.setTitle("Friends");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException | ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    protected void quoteaffirmationButtonClick() throws IOException {
-
-        try {
-            Stage stage = (Stage) quotesaffirmationBtn.getScene().getWindow();
-
-            QuotesAffirmations.loadQuotesAffirmationsScene(stage);
-        }
-
-        catch (IOException e) {
-
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(UserLogin.class.getResource("ArticleView.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 1218, 738);
-        stage.setTitle("Articles");
-        stage.setScene(scene);
-        stage.show();
     }
 
 
